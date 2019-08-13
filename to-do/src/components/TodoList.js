6d64ba07c43d7ec22188d7af78d354efafff61b2
antilou86 <antilou86@gmail.com>
2019-08-13 15:21:09 -0400

    WIP: added extremely basic styling for list items.

---
+++ to-do/src/components/TodoList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Todo from './Todo';
+
+    const TodoList = props => {
+        return(
+            <div className= "task-item-list">
+                {props.todoData.map(item => (
+                    <Todo key={item.id} todo={item} toggleItem={props.toggleItem}/>
+                ))}
+                <button className="clear-btn" onClick={props.clearCompleted}>
+        Clear Completed
+      </button>
+            </div>
+        )
+    }
+
+
+export default  TodoList
